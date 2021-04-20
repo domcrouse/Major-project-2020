fa85db8f0bc1d87269c6cf8e1d960d3ae05852ca
Silenthowler <23553428+domcrouse@users.noreply.github.com>
2021-04-20 23:43:00 +0100

    Majore Project 2021
    Add colliders for the audio
    Work on visual cues
    add flashlight

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    //Plays appropiate audio upon collision
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.tag == "")
+        {
+            FindObjectOfType<AudioManager>().Play("");
+        }
+    }
+}
---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+
+    public AudioClip clip;
+
+
+    //set ranges for each audio clip
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(.1f, 3f)]
+    public float pitch;
+
+    [HideInInspector]
+    public AudioSource source;
+
+    //allows me to loop an audio clip in the inspector
+    public bool loop;
+}
